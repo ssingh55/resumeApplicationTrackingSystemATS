89dc6986d4bf03b180e81078e381996304882287
Shubham Singh <suvamsingh55@gmail.com>
2024-01-18 10:33:04 +0530

    Fixed spacing and some bugfixes in github workflows

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ from PIL import Image
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 def get_gemini_response(input,pdf_content,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
-    response=model.generate_content([input,pdf_content[0],prompt])
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,pdf_content[0],prompt])
     return response.text
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
         ## Convert the pdf to image
-        images=pdf2image.convert_from_bytes(uploaded_file.read())
+        images = pdf2image.convert_from_bytes(uploaded_file.read())
 
-        first_page=images[0]
+        first_page = images[0]
 
         ## Convert to bytes
         img_byte_arr = io.BytesIO()
@@ -48,8 +48,8 @@ def input_pdf_setup(uploaded_file):
     
 st.set_page_config(page_title="ATS Resume Expert")
 st.header("ATS Tracking System")
-input_text=st.text_area("Job Description: ",key="input")
-uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
+input_text = st.text_area("Job Description: ", key="input")
+uploaded_file = st.file_uploader("Upload your resume(PDF)...", type=["pdf"])
 
 if uploaded_file is not None:
     st.write("PDF Uploaded Successfully")
@@ -68,7 +68,7 @@ Please share your professional evaluation on whether the candidate's profile ali
 Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements
 """
 
-input_prompt2="""
+input_prompt2 = """
 Act as a Technical Human Resource Manager and provide guidance on how to improve skills in a 
 specific field based on the user's job description. Conduct a comprehensive analysis of the 
 user's current skills and competencies, as well as the requirements and trends within their 
@@ -79,7 +79,7 @@ can accelerate skill growth. Ensure that the recommendations align with the user
 and provide a clear roadmap for skill enhancement.
 """
 
-input_prompt3="""
+input_prompt3 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of any one job role 
 Data Science,Full Stack web development,devops, big data engineering,Data Analyst and deep ATS functionality,
  your task is to evaluate the resume against the provided job description. Give me the percentage of 
@@ -89,8 +89,8 @@ Data Science,Full Stack web development,devops, big data engineering,Data Analys
 
 if submit1:
     if uploaded_file is not None:
-        pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt1,pdf_content,input_text)
         st.subheader("The Response is")
         st.write(response)
     else:
@@ -98,8 +98,8 @@ if submit1:
 
 elif submit2:
     if uploaded_file is not None:
-        pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt2,pdf_content,input_text)
         st.subheader("The Response is")
         st.write(response)
     else:
@@ -107,10 +107,9 @@ elif submit2:
 
 elif submit3:
     if uploaded_file is not None:
-        pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt3,pdf_content,input_text)
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt3,pdf_content,input_text)
         st.subheader("The Response is")
         st.write(response)
     else:
         st.write("Please upload the resume")
-
